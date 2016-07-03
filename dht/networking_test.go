225d88b5947bfbc43338dcf64abb4cbf43d016c4
Bryce Neal <brycedneal@gmail.com>
2016-07-03 15:07:25 -0700

    Fix mock networking

--- dht/networking_test.go
+++ dht/networking_test.go
@@ -1,37 +1,41 @@
 package dht
 
-// type mockNetworking struct {
-// 	recv chan ([]*message)
-// 	send chan ([]*message)
-// }
-//
-// func newMockNetworking() *mockNetworking {
-// 	net := &mockNetworking{}
-// 	return net
-// }
-//
-// func (net *mockNetworking) listen() error {
-// 	return nil
-// }
-//
-// func (net *mockNetworking) disconnect() error {
-// 	return nil
-// }
-//
-// func (net *mockNetworking) createSocket(host string, port string) error {
-// 	return nil
-// }
-//
-// func (net *mockNetworking) init() {
-// 	net.recv = make(chan ([]*message))
-// 	net.send = make(chan ([]*message))
-// }
-//
-// func (net *mockNetworking) getMessage() (*message, error) {
-// 	return nil, nil
-// }
-//
-// func (net *mockNetworking) sendMessages(q []*message, expectResponse bool) ([]*message, error) {
-// 	net.recv <- q
-// 	return <-net.send, nil
-// }
+type mockNetworking struct {
+	recv chan (*message)
+	send chan (*message)
+}
+
+func newMockNetworking() *mockNetworking {
+	net := &mockNetworking{}
+	return net
+}
+
+func (net *mockNetworking) listen() error {
+	return nil
+}
+
+func (net *mockNetworking) disconnect() error {
+	return nil
+}
+
+func (net *mockNetworking) createSocket(host string, port string) error {
+	return nil
+}
+
+func (net *mockNetworking) init() {
+	net.recv = make(chan (*message))
+	net.send = make(chan (*message))
+}
+
+func (net *mockNetworking) getMessageFin() {
+
+}
+
+func (net *mockNetworking) getMessage() *message {
+	return nil
+}
+
+func (net *mockNetworking) sendMessage(q *message, id int64, expectResponse bool) (chan (*message), error) {
+	net.recv <- q
+	return net.send, nil
+}
